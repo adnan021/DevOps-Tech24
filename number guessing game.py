769dcbfa62594877e184e4ac3788c2a5431062a9
Muhammad Adnan <adnan021@gmail.com>
2024-08-04 18:42:18 +0100

    second commit

--- number guessing game.py
+++ number guessing game.py
@@ -6,7 +6,7 @@ Created on Mon Jul 29 17:03:33 2024
 import random
 
 n = random.randrange(1, 10)
-guess = int(input("Enter any number: "))
+guess = int(input("Please Enter any number: "))
 while n!= guess:
     if guess < n:
         print("Too low")
